740aac9e141627a802079b1aaa89761a08e4c994
tosincoker26 <tosincoker26@gmail.com>
2022-05-16 16:39:21 +0100

    CSS Resets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import './fonts/segoe-ui-4-cufonfonts-webfont/style.css';
 
+import CssBaseline from '@mui/material/CssBaseline';
+
 import { Logo } from './components/ui/Logo';
 import Search from './components/ui/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      <Logo />
-      <Search />
-    </div>
+    <>
+      <CssBaseline />
+      <div className="App">
+        <Logo />
+        <Search />
+      </div>
+    </>
   );
 }
 
